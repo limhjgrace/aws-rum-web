18ab3d201743600f15896ccca8418e17b20e29d3
Grace Lim <limhye@amazon.com>
2024-06-11 19:19:49 -0700

    fix: Create separate loader files for smoke tests

--- smoke/smoke-test-application-NPM-CJS/src/loader-npm-rum.ts
+++ smoke/smoke-test-application-NPM-CJS/src/loader-npm-rum.ts
@@ -1,67 +1,32 @@
 // @ts-nocheck
 const { AwsRum, AwsRumConfig } = require('aws-rum-web');
-const { delay } = require('../../utils');
 
 let awsRum;
-let awsRum2;
 
-(async () => {
-    try {
-        const config: AwsRumConfig = {
-            sessionSampleRate: 1,
-            identityPoolId: $IDENTITY_POOL,
-            endpoint: $ENDPOINT,
-            telemetries: ['performance', 'errors', 'http', 'interaction'],
-            allowCookies: true,
-            enableXRay: true,
-            cookieAttributes: {
-                unique: true
-            }
-        };
+try {
+    const config: AwsRumConfig = {
+        sessionSampleRate: 1,
+        identityPoolId: $IDENTITY_POOL,
+        endpoint: $ENDPOINT,
+        telemetries: ['performance', 'errors', 'http', 'interaction'],
+        allowCookies: true,
+        enableXRay: true,
+        cookieAttributes: {
+            unique: true
+        }
+    };
 
-        const APPLICATION_ID: string = $MONITOR_ID;
-        const APPLICATION_VERSION: string = '1.0.0';
-        const APPLICATION_REGION: string = $REGION;
+    const APPLICATION_ID: string = $MONITOR_ID;
+    const APPLICATION_VERSION: string = '1.0.0';
+    const APPLICATION_REGION: string = $REGION;
 
-        awsRum: AwsRum = new AwsRum(
-            APPLICATION_ID,
-            APPLICATION_VERSION,
-            APPLICATION_REGION,
-            config
-        );
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-
-    // add wait time to ensure credentials are retrieved separately
-    await delay(10000);
-
-    try {
-        const config: AwsRumConfig = {
-            sessionSampleRate: 1,
-            identityPoolId: $IDENTITY_POOL_2,
-            endpoint: $ENDPOINT,
-            telemetries: ['performance', 'errors', 'http', 'interaction'],
-            allowCookies: true,
-            enableXRay: true,
-            cookieAttributes: {
-                unique: true
-            }
-        };
-
-        const APPLICATION_ID: string = $MONITOR_ID_2;
-        const APPLICATION_VERSION: string = '1.0.0';
-        const APPLICATION_REGION: string = $REGION;
-
-        awsRum2: AwsRum = new AwsRum(
-            APPLICATION_ID,
-            APPLICATION_VERSION,
-            APPLICATION_REGION,
-            config
-        );
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-})();
+    awsRum: AwsRum = new AwsRum(
+        APPLICATION_ID,
+        APPLICATION_VERSION,
+        APPLICATION_REGION,
+        config
+    );
+} catch (error) {
+    console.log(error);
+    throw error;
+}
--- smoke/utils.ts
+++
@@ -1,3 +0,0 @@
-export async function delay(milliseconds: number): Promise<void> {
-    await new Promise((resolve) => setTimeout(resolve, milliseconds));
-}
